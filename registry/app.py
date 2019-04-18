943fbb8899821f026f86753a02814f4b1ab95231
Ilton Garcia dos Santos Silveira <iltonsilveira91@gmail.com>
2019-04-18 12:21:34 -0300

    retry registry to eureka and anvisa micro service dependency added

--- registry/app.py
+++ registry/app.py
@@ -1,30 +1,35 @@
 import os
+import urllib
+from time import sleep
 import py_eureka_client.eureka_client as eureka_client
 
-# TODO: retrieve config
-# http://discovery:8761/configuration/autosprocessuais-pecas-textos-default.json
-
-# no spaces or underscores, this needs to be url-friendly
-service_name = 'ui'
-
-# This needs to be an IP accessible by anyone that
-# may want to discover, connect and/or use your service.
-ip = 'localhost' #'127.0.0.1'
-port = 5000
-
-home_page_url = 'manage/info'
-manage_health_path = 'manage/health'
-eureka_url = 'http://discovery:8761/eureka'
-
-your_rest_server_port = 5000
 # The flowing code will register your server to eureka server and also start to send heartbeat every 30 seconds
-registry_client, discovery_client = eureka_client.init(eureka_server=eureka_url,
-                                                       app_name=service_name,
-                                                       instance_ip=ip,
-                                                       instance_port=your_rest_server_port)
+def registry():
+    # no spaces or underscores, this needs to be url-friendly
+    service_name = 'ui'
 
-registry_client.send_heart_beat()
-# discovery_client.start()
+    # This needs to be an IP accessible by anyone that
+    # may want to discover, connect and/or use your service.
+    flask_port = 5000
+    ip = 'localhost' #'127.0.0.1'
+    your_rest_server_port = 8447
+
+    home_page_url = 'manage/info'
+    manage_health_path = 'manage/health'
+    eureka_url = 'http://discovery:8761/eureka'
+
+    try:
+        # The flowing code will register your server to eureka server and also start to send heartbeat every 30 seconds
+        registry_client, discovery_client = eureka_client.init(eureka_server=eureka_url,
+                                                               app_name=service_name,
+                                                               instance_ip=ip,
+                                                               instance_port=your_rest_server_port)
+
+        registry_client.send_heart_beat()
+        # discovery_client.start()
+    except Exception as e:
+        sleep(5)
+        return registry()
 
 import json
 from flask import Flask, send_from_directory
