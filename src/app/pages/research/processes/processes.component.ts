64878d327d0b9ee48fdf85d0f5d7ca26ef7bebd7
ArthurJahn <stutrzbecher@gmail.com>
2019-10-30 22:59:58 -0300

    add example for pdf viewer in processes modal

--- src/app/pages/research/processes/processes.component.ts
+++ src/app/pages/research/processes/processes.component.ts
@@ -11,12 +11,18 @@ import {debounceTime} from 'rxjs/operators';
 export class SProcessesComponent implements OnInit {
 
   public processes = [];
+  public pdf: String;
 
   constructor() {}
 
   ngOnInit() {
     this.processes = [
-      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: '', status: 'concluído' },
+      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OYlQxVHBSYTlZNi1uT0FSOTZNMGdGbFUxSG1z', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Assistência Farmacêutica 4.0  caminhos para a implementação.pdf` },
     ];
- }
+  }
+  setPdf(pdf) {
+    setTimeout(()=> {
+      this.pdf = pdf;
+    }, 200);
+  }
 }
