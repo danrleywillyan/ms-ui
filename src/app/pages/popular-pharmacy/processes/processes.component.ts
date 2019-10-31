c5816fe635823cd609d822565873d9875e96fa03
ArthurJahn <stutrzbecher@gmail.com>
2019-10-31 00:25:05 -0300

    add popfarm ai analysis

--- src/app/pages/popular-pharmacy/processes/processes.component.ts
+++ src/app/pages/popular-pharmacy/processes/processes.component.ts
@@ -17,10 +17,10 @@ export class ProcessesComponent implements OnInit {
 
   ngOnInit() {
     this.processes = [
-      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OYlQxVHBSYTlZNi1uT0FSOTZNMGdGbFUxSG1z', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Assistência Farmacêutica 4.0  caminhos para a implementação.pdf` },
-      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OdXMxYkRlNzMtRzVPbkpRSjlKZmRBNWxZckNn', status: 'concluído', pdf: '' },
-      { title: 'Validar Dados do Cadastro da Farmácia [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OaGdkVzhRYVF2Q1RCYUZsMHVYTkdGOGlSZHBB', status: 'concluído', pdf: '' },
-      { title: 'Realizar teste em homologação [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-ORVlzOVdXSV9YM3lQclZRbGlsOHpQRk9SZWhB', status: 'concluído', pdf: '' },
+      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OYlQxVHBSYTlZNi1uT0FSOTZNMGdGbFUxSG1z', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
+      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OdXMxYkRlNzMtRzVPbkpRSjlKZmRBNWxZckNn', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
+      { title: 'Validar Dados do Cadastro da Farmácia [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OaGdkVzhRYVF2Q1RCYUZsMHVYTkdGOGlSZHBB', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
+      { title: 'Realizar teste em homologação [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-ORVlzOVdXSV9YM3lQclZRbGlsOHpQRk9SZWhB', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
     ];
   }
   setPdf(pdf) {
