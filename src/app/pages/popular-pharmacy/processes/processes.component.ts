16aca6c37eb1c2d0ba136205d3e0f053ebba58f2
ArthurJahn <stutrzbecher@gmail.com>
2019-10-31 00:58:47 -0300

    add all processes

--- src/app/pages/popular-pharmacy/processes/processes.component.ts
+++ src/app/pages/popular-pharmacy/processes/processes.component.ts
@@ -17,11 +17,45 @@ export class ProcessesComponent implements OnInit {
 
   ngOnInit() {
     this.processes = [
-      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OYlQxVHBSYTlZNi1uT0FSOTZNMGdGbFUxSG1z', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
-      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OdXMxYkRlNzMtRzVPbkpRSjlKZmRBNWxZckNn', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
-      { title: 'Validar Dados do Cadastro da Farmácia [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OaGdkVzhRYVF2Q1RCYUZsMHVYTkdGOGlSZHBB', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
-      { title: 'Realizar teste em homologação [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-ORVlzOVdXSV9YM3lQclZRbGlsOHpQRk9SZWhB', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/` },
+      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OdXMxYkRlNzMtRzVPbkpRSjlKZmRBNWxZckNn', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/02_02 Ressarcimento.pdf` },
+      { title: 'Solicitar Credenciamento no PFPB [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OYlQxVHBSYTlZNi1uT0FSOTZNMGdGbFUxSG1z', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/01_01 Credenciamento.pdf` },
+      { title: 'Validar Dados do Cadastro da Farmácia [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OaGdkVzhRYVF2Q1RCYUZsMHVYTkdGOGlSZHBB', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/01_01_01 Validar Dados da Farmácia.pdf` },
+      { title: 'Realizar teste em homologação [TO BE]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-ORVlzOVdXSV9YM3lQclZRbGlsOHpQRk9SZWhB', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/01_01_02 Teste em Homologação.pdf` },
+      { title: 'Solicitar fusão e incorporação de empresa no PFPB [TO BE]', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Renovar Credenciamento de Empresa', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Dispensar medicamentos pelas empresas credenciadas no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Realizar monitoramento eletrônico das empresas credenciadas no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Receber denúncias para apuração relativas ao PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Apurar irregularidades ou indícios de fraude no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Requerer documentação impressa de empresas credenciadas no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Realizar auditoria em empresas credenciadas no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Multar empresa credenciada no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Realizar cobrança administrativa de farmácia credenciada no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Realizar cobrança administrativa de farmácia credenciada no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Tratar empresa inadimplente no PFPB', url: '', status: 'a fazer', pdf: `` },
+      { title: 'Descredenciar empresa por irregularidade no PFPB', url: '', status: 'a fazer', pdf: `` },
+
     ];
+    /*
+ - Solicitar credenciamento no PFPB
+Solicitar fusão e incorporação de empresa no PFPB
+ - Realizar credenciamento de empresa no PFPB
+Renovar Credenciamento de Empresa
+Dispensar medicamentos pelas empresas credenciadas no PFPB
+Preparar ressarcimento mensal do PFPB
+Preparar ressarcimento de processos pendentes no PFPB
+Realizar monitoramento eletrônico das empresas credenciadas no PFPB
+Receber denúncias para apuração relativas ao PFPB
+Apurar irregularidades ou indícios de fraude no PFPB
+Requerer documentação impressa de empresas credenciadas no PFPB
+Realizar auditoria em empresas credenciadas no PFPB
+Multar empresa credenciada no PFPB
+Realizar cobrança administrativa de farmácia credenciada no PFPB
+Tratar empresa inadimplente no PFPB
+Descredenciar empresa por irregularidade no PFPB
+Planejar PFPB
+Controlar PFPB
+    */
   }
   setPdf(pdf) {
     setTimeout(()=> {
--- src/app/pages/research/processes/processes.component.ts
+++ src/app/pages/research/processes/processes.component.ts
@@ -17,7 +17,12 @@ export class SProcessesComponent implements OnInit {
 
   ngOnInit() {
     this.processes = [
-      { title: 'Preparar ressarcimento mensal do PFPB [AS IS]', url: 'https://drive.google.com/open?id=0B1neC5Yco3-OYlQxVHBSYTlZNi1uT0FSOTZNMGdGbFUxSG1z', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Assistência Farmacêutica 4.0  caminhos para a implementação.pdf` },
+      { title: 'Elaborar Termo de Referência', url: 'https://drive.google.com/open?id=153Wk1Pm0ph1g1vO-SvzQ3-_Ryay0hq3J', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Coord - Elaborar Termo de Referência v2.pdf` },
+      { title: 'Monitorar Estoque_Calamidade Pública', url: 'https://drive.google.com/open?id=153Wk1Pm0ph1g1vO-SvzQ3-_Ryay0hq3J', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Coord - Monitorar Estoque_Calamidade Pública.pdf` },
+      { title: 'Monitorar Estoque_Saúde da Mulher e Insulina', url: 'https://drive.google.com/open?id=153Wk1Pm0ph1g1vO-SvzQ3-_Ryay0hq3J', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Coord - Monitorar Estoque_Saúde da Mulher e Insulina.pdf` },
+      { title: 'Planejar Programação_Saúde da Mulher e Insulina', url: 'https://drive.google.com/open?id=153Wk1Pm0ph1g1vO-SvzQ3-_Ryay0hq3J', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Coord - Planejar Programação_Saúde da Mulher e Insulina.pdf` },
+      { title: 'Realizar Aquisição Centralizada por Pregão e IRP', url: 'https://drive.google.com/open?id=153Wk1Pm0ph1g1vO-SvzQ3-_Ryay0hq3J', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Coord - Realizar Aquisição Centralizada por Pregão e IRP.pdf` },
+      { title: 'Realizar Aquisição Centralizada v2', url: 'https://drive.google.com/open?id=153Wk1Pm0ph1g1vO-SvzQ3-_Ryay0hq3J', status: 'concluído', pdf: `http://${window.location.host}/static/assets/pdfs/Coord - Realizar Aquisição Centralizada v2.pdf` },
     ];
   }
   setPdf(pdf) {
